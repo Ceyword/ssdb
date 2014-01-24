0be5aa527c12b24a06f3ad26d117ed9d0764fb6b
Ceyword <ceyword@hotmail.com>
2014-01-24 02:13:36 +0100

    Added asynchrony to the c# client

--- api/dotnet/Link.cs
+++ api/dotnet/Link.cs
@@ -1,54 +1,102 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
+ 
 using System.Net;
 using System.Net.Sockets;
 using System.IO;
-
+using System.Security.Cryptography;
 namespace ssdb
 {
-	class Link
-	{
-		private TcpClient sock;
-		private MemoryStream recv_buf = new MemoryStream(8 * 1024);
+    class Link
+    {
+	private TcpClient sock;
+        private MemoryStream recv_buf = new MemoryStream(8 * 1024);
 
-		public Link(string host, int port) {
-			sock = new TcpClient(host, port);
-			sock.NoDelay = true;
-			sock.Client.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.KeepAlive, true);
-		}
+        public Link(string host, int port)
+        {
+            sock = new TcpClient(host, port);
+            sock.NoDelay = true;
+            sock.Client.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.KeepAlive, true);
+        }
 
-		~Link() {
-			this.close();
-		}
+        ~Link()
+        {
+            this.close();
+        }
 
-		public void close() {
-			if(sock != null){
-				sock.Close();
-			}
-			sock = null;
-		}
+        public void close()
+        {
+            if (sock != null)
+            {
+                sock.Close();
+            }
+            sock = null;
+        }
 
-		public List<byte[]> request(string cmd, params string[] args) {
+        public List<byte[]> request(string cmd, params string[] args)
+        {
+            List<byte[]> req = new List<byte[]>(1 + args.Length);
+            req.Add(Encoding.Default.GetBytes(cmd));
+            foreach (string s in args)
+            {
+                req.Add(Encoding.Default.GetBytes(s));
+            }
+            return this.request(req);
+        }
+		public async Task<List<byte[]>> requestAsync(string cmd, params string[] args)
+		{
 			List<byte[]> req = new List<byte[]>(1 + args.Length);
 			req.Add(Encoding.Default.GetBytes(cmd));
-			foreach(string s in args) {
+			foreach (string s in args)
+			{
 				req.Add(Encoding.Default.GetBytes(s));
 			}
-			return this.request(req);
+			return await this.requestAsync(req);
 		}
 
-		public List<byte[]> request(string cmd, params byte[][] args) {
+        public List<byte[]> request(string cmd, params byte[][] args)
+        {
+            List<byte[]> req = new List<byte[]>(1 + args.Length);
+            req.Add(Encoding.Default.GetBytes(cmd));
+            req.AddRange(args);
+            return this.request(req);
+        }
+		public async Task<List<byte[]>> requestAsync(string cmd, params byte[][] args)
+		{
 			List<byte[]> req = new List<byte[]>(1 + args.Length);
 			req.Add(Encoding.Default.GetBytes(cmd));
 			req.AddRange(args);
-			return this.request(req);
+			return await this.requestAsync(req);
 		}
 
-		public List<byte[]> request(List<byte[]> req) {
-			MemoryStream buf = new MemoryStream();
-			foreach(byte[] p in req) {
+        public List<byte[]> request(List<byte[]> req)
+        {
+			MemoryStream buf = new MemoryStream(); byte[] bs = requestcommon(req, buf);
+			sock.GetStream().Write(bs, 0, (int)buf.Length);
+            return recv();
+        }
+		public async Task<List<byte[]>> requestAsync(List<byte[]> req)
+		{
+			MemoryStream buf = new MemoryStream(); 
+			byte[] bs = requestcommon(req, buf);
+			Task<List<byte[]>> parent = Task<List<byte[]>>.Factory.StartNew(() =>
+			{
+				sock.GetStream().WriteAsync(bs, 0, (int)buf.Length);
+			    Task<List<byte[]>>  child = Task<List<byte[]>>.Factory.StartNew(() => { return recvAsync().Result; }, TaskCreationOptions.AttachedToParent);
+				return child.Result;
+			}, TaskCreationOptions.PreferFairness);
+
+			return await parent;
+
+		}
+
+		private byte[] requestcommon(List<byte[]> req, MemoryStream buf)
+		{
+			foreach (byte[] p in req)
+			{
 				byte[] len = Encoding.Default.GetBytes(p.Length.ToString());
 				buf.Write(len, 0, len.Length);
 				buf.WriteByte((byte)'\n');
@@ -58,74 +106,106 @@ namespace ssdb
 			buf.WriteByte((byte)'\n');
 
 			byte[] bs = buf.GetBuffer();
-			sock.GetStream().Write(bs, 0, (int)buf.Length);
 			//Console.Write(Encoding.Default.GetString(bs, 0, (int)buf.Length));
-			return recv();
-		}
+            return bs;
 
-		private List<byte[]> recv() {
-			while(true) {
+		}
+		
+
+        private List<byte[]> recv()
+        {
+            while (true)
+            {
+                List<byte[]> ret = parse();
+                if (ret != null)
+                {
+                    return ret;
+                }
+                byte[] bs = new byte[8192];
+                int len = sock.GetStream().Read(bs, 0, bs.Length);
+                //Console.WriteLine("<< " + Encoding.Default.GetString(bs));
+                recv_buf.Write(bs, 0, len);
+            }
+        }
+		public async Task<List<byte[]>> recvAsync()
+		{
+			while (true)
+			{
 				List<byte[]> ret = parse();
-				if(ret != null) {
+				if (ret != null)
+				{
 					return ret;
 				}
 				byte[] bs = new byte[8192];
-				int len = sock.GetStream().Read(bs, 0, bs.Length);
+				int len = await sock.GetStream().ReadAsync(bs, 0, bs.Length);
 				//Console.WriteLine("<< " + Encoding.Default.GetString(bs));
-				recv_buf.Write(bs, 0, len);
+				recv_buf.Write(bs, 0, len);				
 			}
 		}
 
-		private static int memchr(byte[] bs, byte b, int offset) {
-			for(int i = offset; i < bs.Length; i++) {
-				if(bs[i] == b) {
-					return i;
-				}
-			}
-			return -1;
-		}
+        private static int memchr(byte[] bs, byte b, int offset)
+        {
+            for (int i = offset; i < bs.Length; i++)
+            {
+                if (bs[i] == b)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
 
-		private List<byte[]> parse() {
-			List<byte[]> list = new List<byte[]>();
-			byte[] buf = recv_buf.GetBuffer();
+        private List<byte[]> parse()
+        {
+            List<byte[]> list = new List<byte[]>();
+            byte[] buf = recv_buf.GetBuffer();
 
-			int idx = 0;
-			while(true) {
-				int pos = memchr(buf, (byte)'\n', idx);
-				//System.out.println("pos: " + pos + " idx: " + idx);
-				if(pos == -1) {
-					break;
-				}
-				if(pos == idx || (pos == idx + 1 && buf[idx] == '\r')) {
-					idx += 1; // if '\r', next time will skip '\n'
-					// ignore empty leading lines
-					if(list.Count == 0) {
-						continue;
-					} else {
-						int left = (int)recv_buf.Length - idx;
-						recv_buf = new MemoryStream(8192);
-						if(left > 0) {
-							recv_buf.Write(buf, idx, left);
-						}
-						return list;
-					}
-				}
-				byte[] lens = new byte[pos - idx];
-				Array.Copy(buf, idx, lens, 0, lens.Length);
-				int len = Int32.Parse(Encoding.Default.GetString(lens));
+            int idx = 0;
+            while (true)
+            {
+                int pos = memchr(buf, (byte)'\n', idx);
+                //System.out.println("pos: " + pos + " idx: " + idx);
+                if (pos == -1)
+                {
+                    break;
+                }
+				if (pos == idx || (pos == idx + 1 && buf[idx] == '\r'))
+                {
+                    idx += 1; // if '\r', next time will skip '\n'
+                    // ignore empty leading lines
+                    if (list.Count == 0)
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        int left = (int)recv_buf.Length - idx;
+                        recv_buf = new MemoryStream(8192);
+                        if (left > 0)
+                        {
+                            recv_buf.Write(buf, idx, left);
+                        }
+                        return list;
+                    }
+                }
+                byte[] lens = new byte[pos - idx];
+                Array.Copy(buf, idx, lens, 0, lens.Length);
+                int len = Int32.Parse(Encoding.Default.GetString(lens));
 
-				idx = pos + 1;
-				if(idx + len >= recv_buf.Length) {
-					break;
-				}
-				byte[] data = new byte[len];
-				Array.Copy(buf, idx, data, 0, (int)data.Length);
+                idx = pos + 1;
+                if (idx + len >= recv_buf.Length)
+                {
+                    break;
+                }
+                byte[] data = new byte[len];
+                Array.Copy(buf, idx, data, 0, (int)data.Length);
 
-				//Console.WriteLine("len: " + len + " data: " + Encoding.Default.GetString(data));
-				idx += len + 1; // skip '\n'
-				list.Add(data);
-			}
-			return null;
-		}
-	}
+                //Console.WriteLine("len: " + len + " data: " + Encoding.Default.GetString(data));
+                idx += len + 1; // skip '\n'
+                list.Add(data);
+            }
+            return null;
+        }
+    }
 }
+
